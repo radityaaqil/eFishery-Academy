33916da8826419ee734b4e92570329e494f367a6
radityaaqil <radityaaqil@gmail.com>
2022-08-23 09:18:56 +0700

    first day

---
+++ practice_go.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World")
+
+	var name string
+	var name1 string
+	name = "eFishery"
+	name1 = " aqua developer"
+
+	fmt.Println(name+name1)
+
+	var a int16
+	var b int16
+
+	a = 100
+	b = 132
+
+	fmt.Println(a+b)
+}
