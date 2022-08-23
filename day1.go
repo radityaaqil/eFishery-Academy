3f54c25fc26aae73a4c7674ff30674ebe6f752d6
radityaaqil <radityaaqil@gmail.com>
2022-08-23 11:45:16 +0700

    second day commit

--- day1.go
+++ day1.go
@@ -15,3 +15,17 @@ package main
 //git commit -m --> with message
 
 //GIT PUSH
+
+//GIT PULL
+
+//GIT MERGE
+
+//GIT KARMA CONVENTION (Commit Message Structure)
+//see web for details
+
+//SEMANTIC VERSIONING
+//version x.y.z
+//x major, y minor, z patch
+
+//GIT MANAGEMENT
+//TRUNK BASED DEVELOPMENT --> single branch (main or master)
