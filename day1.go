8824176d493f93048d64ae530b6f7e4dc88dc4da
radityaaqil <radityaaqil@gmail.com>
2022-08-23 10:32:55 +0700

    MAIN

--- day1.go
+++ day1.go
@@ -1,17 +1,3 @@
 package main
 
-//VERSION CONTROL
-
-// GIT INIT
-//git init --bare --> not hidden
-//git init --quiet --> hidden
-
-//GIT ADD
-//git add . --> all files
-//git add <filename> --> specific file
-
-//GIT COMMIT
-//git commit -a --> all
-//git commit -m --> with message
-
-//GIT PUSH
+//MAIN
